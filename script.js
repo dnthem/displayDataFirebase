3eeeae41e45a10484275f94200a62eef41cec70f
Them Dang <dnthem@gmail.com>
2021-12-30 23:09:20 -0800

    update constraints

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getDatabase, ref, onValue, update } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getDatabase, ref, onValue, update, set } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,6 +24,92 @@ let FLAG_PICK_UP_DATE = false;
 let FLAG_PICK_UP_TODAY = false;
 let DATE = "";
 
+const menu = [
+    {
+      index: 0,
+      id: "bttron",
+      name: "Bánh Tráng Trộn",
+      price: 10,
+    },
+    {
+      index: 1,
+      id: "btcuon",
+      name: "Bánh Tráng Cuốn",
+      price: 10,
+    },
+    {
+      index: 2,
+      id: "btsate",
+      name: "Bánh Tráng Sate",
+      price: 7,
+    },
+    {
+      index: 3,
+      id: "trungNuong",
+      name: "Trứng Nướng (5 trứng/phần)",
+      price: 7,
+    },
+    {
+      index: 4,
+      id: "dauhu",
+      name: "Đậu Hũ Nước Dừa",
+      price: 7,
+    },
+    {
+      index: 5,
+      id: "bapxao",
+      name: "Bắp Xào",
+      price: 10,
+    },
+  ];
+
+
+function setMenu() {
+    const menuSection = document.getElementById("menu");
+    menu.forEach(obj => {
+        const label = document.createElement("label");
+        const option = document.createElement("input");
+        const br = document.createElement("br")
+        label.innerText = obj.name;
+        option.type = "checkbox";
+        option.value = obj.index;
+        menuSection.appendChild(option);
+        menuSection.appendChild(label);
+        
+        menuSection.appendChild(br);
+        menuSection.appendChild(br);
+    })
+}
+
+function getIndices(checkBoxes) {
+    let list = [];
+    
+    checkBoxes.forEach(box => {
+        if (box.checked == true) list.push(box.value);
+    })
+    return list;
+}
+
+function updateSetting () {
+    let updateObj = {};
+    let indexList = [];
+    const checkBoxes = document.querySelectorAll("input[type='checkbox']");
+    indexList = getIndices(checkBoxes);
+    
+    const date = document.querySelector("#set-pick-up-date").value;
+    updateObj = {
+        date : date,
+        indexList : indexList
+    }
+
+    set(ref(db, '/constraint/'),updateObj)
+        .then(() => {
+            alert("Update constraints successful");
+        })
+        .catch((err) => {
+            alert(err + ":" + err.message);
+        })
+}
 
 function init () {
     const flag = localStorage.getItem("Flag");
@@ -32,34 +118,10 @@ function init () {
     FLAG_PICK_UP_DATE = (flag == "true")? true : false;
     FLAG_PICK_UP_TODAY = (flag2 == "true")? true : false;
     DATE = (date != null)? date : "0";
+    setMenu();
 }
 
-window.addEventListener("load", init);
-onValue(updateRef, (snapshot) => {
-    let index = 0;
-    const data = snapshot.val();
-    const table = document.querySelector("#table-content");
-    table.innerHTML = `
-    <tr>
-        <th>#</th>
-        <th>Name</th>
-        <th>Phone</th>
-        <th>Order</th>
-        <th>Pick-up Date</th>
-        <th>Pick-up Time</th>
-        <th>Payment</th>
-        <th>Total</th>
-        <th>Note</th>
-        <th>Status</th>
-        <th>Check</th>
-    </tr>`;
-    for (const key in data) {
-        const order = data[key];
-            setTable(index, table,order, key);
-        index++;
-    }
-    
-});
+
 
 function setTable (index, table, order, key) {
     if (FLAG_PICK_UP_DATE) {
@@ -143,6 +205,28 @@ function bindEventFinish (button) {
     });
 }
 
+function getDate () {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    return yyyy + "-" + mm + "-" + dd;
+}
+
+document.querySelector("#setting").addEventListener("click", (event) => {
+    if (event.target.innerText === "Setting") {
+        document.querySelector("#table-content").classList.add("hidden");
+        document.querySelector(".setting").classList.remove("hidden");
+        event.target.innerText = "Go back";
+    }
+    else {
+        document.querySelector("#table-content").classList.remove("hidden");
+        document.querySelector(".setting").classList.add("hidden");
+        event.target.innerText = "Setting"
+    }
+})
+
 document.querySelector("#order-by-date").addEventListener("click", () => {
     localStorage.setItem("Flag", "true");
     localStorage.setItem("Flag2", "false");
@@ -164,11 +248,33 @@ document.querySelector("#order-today").addEventListener("click", () => {
     location.reload();
 });
 
-function getDate () {
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
+document.querySelector("#update-btn").onclick = updateSetting;
+window.addEventListener("load", init);
+onValue(updateRef, (snapshot) => {
+    let index = 0;
+    const data = snapshot.val();
+    const table = document.querySelector("#table-content");
+    table.innerHTML = `
+    <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Phone</th>
+        <th>Order</th>
+        <th>Pick-up Date</th>
+        <th>Pick-up Time</th>
+        <th>Payment</th>
+        <th>Total</th>
+        <th>Note</th>
+        <th>Status</th>
+        <th>Check</th>
+    </tr>`;
+    for (const key in data) {
+        const order = data[key];
+            setTable(index, table,order, key);
+        index++;
+    }
+    
+});
 
-    return yyyy + "-" + mm + "-" + dd;
-}
+
+
